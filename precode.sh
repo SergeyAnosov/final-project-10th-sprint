3faa6f8c605304e2acf652add7baf80257a91491
Anosov Sergey <saanosov@istok.ad>
2025-03-27 23:08:13 +0300

    feat: for review

--- precode.sh
+++ precode.sh
@@ -20,8 +20,8 @@ touch dir2/empty.txt
 # #!/bin/bash
 # echo "$1, привет!"
 touch dir2/hello.sh
-echo '#!bin/bash' > dir2/hello.sh
-echo '$1, привет' >> dir2/hello.sh
+echo '#!/bin/bash' > dir2/hello.sh
+echo 'echo "$1, привет!"' >> dir2/hello.sh
 
 #устанавливаем для task/dir2/hello.sh права rwxrw-r--
 chmod 764 dir2/hello.sh
@@ -34,21 +34,30 @@ cp dir2/* dir3/dir4/
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
+find -name "*.txt" > dir1/summary.txt
 
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
+cat dir2/list.txt >> dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
+export NAME='Всем студентам'
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
+./dir2/hello.sh "$NAME"
 
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
+mv dir1/summary.txt "Практическое задание.txt"
 
 # выводим на консоль содержимое файла task/Практическое задание
+cat "Практическое задание.txt"
 
 # ищем в файле "Практическое задание" строки, которые содержат слово "dir"
 # и затем отсортировываем их
+grep "dir" "Практическое задание.txt" | sort
 
 # меняем текущую директорию на родительскую для task
+cd ..
 
 # удаляем директорию task со всем содержимым
+rm -rf task/
